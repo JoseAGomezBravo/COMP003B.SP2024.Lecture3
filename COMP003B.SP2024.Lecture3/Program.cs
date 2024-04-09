559b35d4efa446200bbd9929629bb018d699c311
JoseAGomezBravo <04josegomez2022@gmail.com>
2024-04-08 18:55:53 -0700

    commit changes

--- COMP003B.SP2024.Lecture3/Program.cs
+++ COMP003B.SP2024.Lecture3/Program.cs
@@ -25,7 +25,7 @@ namespace COMP003B.SP2024.Lecture3
             app.UseRouting();
 
             app.UseAuthorization();
-
+            // TODO: modeify controller route to default to StudentsController
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Students}/{action=Index}/{id?}");
